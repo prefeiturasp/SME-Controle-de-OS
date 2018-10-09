7455512bc12770a88b6d7f8dc7b3cd8b58bd56e4
Pamela Bembe Ianovalli <pamela.novalli@gmail.com>
2018-10-09 01:15:32 -0300

    Adicionado banco de dados postgre. Painel administrativo ativo.

--- django/sme/settings.py
+++ django/sme/settings.py
@@ -72,15 +72,18 @@ WSGI_APPLICATION = 'sme.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'osmanagementdb',
+        'USER': 'osmanager',
+        'PASSWORD': 'osos',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
