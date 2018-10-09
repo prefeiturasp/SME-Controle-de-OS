81ff44edbc2d5c8caecaaee41bc57d826b479b43
Pamela Bembe Ianovalli <pamela.novalli@gmail.com>
2018-10-09 03:07:21 -0300

    Adicionado UUID ao banco de dados

--- django/sme/os_management/admin.py
+++ django/sme/os_management/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from sme.os_management.models import Usuario, Divisao, Coordenadoria
 
 # Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+  model:Usuario
+  readonly_fields = ['id_divisao']
+  list_display = ['usuario_nome']
+  search_fields = ['usuario_nome']
+admin.site.register(Usuario, UsuarioAdmin)
+
+class DivisaoAdmin(admin.ModelAdmin):
+  model:Divisao
+  readonly_fields = ['id_divisao']
+  list_display = ['divisao_nome', 'divisao_sigla', 'coordenadoria_nome']
+  search_fields = ['divisao_nome', 'divisao_sigla']
+
+  def coordenadoria_nome(self, instance):
+        return instance.id_coord.coord_nome
+
+admin.site.register(Divisao, DivisaoAdmin)
+
+class CoordenadoriaAdmin(admin.ModelAdmin):
+  model:Coordenadoria
+  readonly_fields = ['id_coord']
+  list_display = ['coord_nome']
+  search_fields = ['coord_nome']
+admin.site.register(Coordenadoria, CoordenadoriaAdmin)
--- django/sme/os_management/models.py
+++ django/sme/os_management/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Coordenadoria(models.Model):
+    id_coord = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    coord_nome = models.CharField(max_length= 100)
+
+class Divisao(models.Model):
+    id_divisao = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id_coord = models.ForeignKey(Coordenadoria, on_delete=models.CASCADE)
+    divisao_sigla = models.CharField(max_length= 6)
+    divisao_nome = models.CharField(max_length= 100)
+
+class Usuario(models.Model):
+    id_usuario = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id_divisao = models.ForeignKey(Divisao, on_delete=models.CASCADE)
+    usuario_login = models.CharField(max_length=10)
+    usuario_nome = models.CharField(max_length=100)
