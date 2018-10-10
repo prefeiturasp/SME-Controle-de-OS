23101bfe5fcfeb7629372cf13e64b93e8b5e17ab
Pamela <pamela.novalli@gmail.com>
2018-10-10 11:54:25 -0300

    Update

--- django/sme/os_management/admin.py
+++ django/sme/os_management/admin.py
@@ -3,14 +3,14 @@ from sme.os_management.models import Usuario, Divisao, Coordenadoria
 
 # Register your models here.
 class UsuarioAdmin(admin.ModelAdmin):
-  model:Usuario
+  #model:Usuario
   readonly_fields = ['id_divisao']
   list_display = ['usuario_nome']
   search_fields = ['usuario_nome']
 admin.site.register(Usuario, UsuarioAdmin)
 
 class DivisaoAdmin(admin.ModelAdmin):
-  model:Divisao
+  #model:Divisao
   readonly_fields = ['id_divisao']
   list_display = ['divisao_nome', 'divisao_sigla', 'coordenadoria_nome']
   search_fields = ['divisao_nome', 'divisao_sigla']
@@ -21,7 +21,7 @@ class DivisaoAdmin(admin.ModelAdmin):
 admin.site.register(Divisao, DivisaoAdmin)
 
 class CoordenadoriaAdmin(admin.ModelAdmin):
-  model:Coordenadoria
+  #model:Coordenadoria
   readonly_fields = ['id_coord']
   list_display = ['coord_nome']
   search_fields = ['coord_nome']
