2eac03d0c9866aa92661bc499449df81f8e96d27
Pamela <pamela.novalli@gmail.com>
2018-11-30 15:51:11 -0200

    Estimating description table created in template estimarOS

--- django/sme/os_management/forms.py
+++ django/sme/os_management/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm, Textarea
 from .models import (MeuLogin, CadastroOS, Administrativo, TermoContrato, TermoAditivo,
-Demandante, TipoServico, Sistema, Fase, Status)
+Demandante, TipoServico, Sistema, Fase, Status, EstimarEsforco)
 
 class MeuLoginForm(forms.ModelForm):
 
@@ -18,10 +18,13 @@ class CadastroOSForm(forms.ModelForm):
     responsavel = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Digite o responsavel por esta solicitação'}))
     tipo = forms.ModelChoiceField(queryset=TipoServico.objects.all(), widget=forms.TextInput(attrs={'placeholder': 'Ex: Reparação, manutenção, nova funcionalidade'}))
     sistema = forms.ModelChoiceField(queryset=Sistema.objects.all(), widget=forms.TextInput(attrs={'placeholder': 'Ex: EOL, SGP, SERAP'}))
+    processo_adm = forms.ModelChoiceField(queryset=Administrativo.objects.all(), widget=forms.TextInput())
+    termo_contrato = forms.ModelChoiceField(queryset=TermoContrato.objects.all(), widget=forms.TextInput())
+    termo_contrato_aditivo= forms.ModelChoiceField(queryset=TermoAditivo.objects.all(), widget=forms.TextInput())
 
     class Meta:
         model = CadastroOS
-        fields = ( 'n_os','data_necessidade','demandante' ,'responsavel', 'tipo', 'prioridade', 'sistema', 'solicitacao', 'observacao')
+        fields = ( 'n_os','data_necessidade','demandante' ,'responsavel', 'processo_adm', 'termo_contrato', 'termo_contrato_aditivo','tipo', 'prioridade', 'sistema', 'solicitacao', 'observacao')
         widgets = {
             'solicitacao': Textarea(attrs={'cols': 20, 'rows': 5, 'placeholder':'Descreva qual foi a necessidade que gerou está solicitação'}),
             'observacao' : Textarea(attrs={'cols': 20, 'rows': 5}),
@@ -30,17 +33,13 @@ class CadastroOSForm(forms.ModelForm):
 
 class EstimarOSForm(forms.ModelForm):
 
-    n_os = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Digite o numero da Ordem de Serviço'}))
-    processo_adm = forms.ModelChoiceField(queryset=Administrativo.objects.all(), widget=forms.TextInput())
-    termo_contrato = forms.ModelChoiceField(queryset=TermoContrato.objects.all(), widget=forms.TextInput())
-    termo_contrato_aditivo = forms.ModelChoiceField(queryset=TermoAditivo.objects.all(), widget=forms.TextInput())    
+    n_os = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Digite o numero da Ordem de Serviço'})) 
     fase = forms.ModelChoiceField(queryset=Fase.objects.all(), widget=forms.TextInput(attrs={'placeholder': 'Ex: Em andamento'}))
     status = forms.ModelChoiceField(queryset=Status.objects.all(), widget=forms.TextInput())
-    
+
     class Meta:
         model = CadastroOS
-        fields = ('n_os', 'termo_contrato', 'termo_contrato_aditivo', 'processo_adm', 'data_aceite','data_entrega','mes_fatura', 'mes_fatura',
-        'ano_fatura')
+        fields = ('n_os','data_aceite','data_entrega',)
    
 class VisualizarOSForm(forms.ModelForm):
     
